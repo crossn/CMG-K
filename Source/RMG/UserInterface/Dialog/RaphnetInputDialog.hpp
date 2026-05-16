d3f3c5e32ffdf204ce2ad2b3d45aa6e393c9a28a
nickthename <nickthename@users.noreply.github.com>
2026-05-16 11:43:45 -0400

    Add raphnet debug stats

--- Source/RMG/UserInterface/Dialog/RaphnetInputDialog.hpp
+++ Source/RMG/UserInterface/Dialog/RaphnetInputDialog.hpp
@@ -18,6 +18,7 @@
 #include <QProgressBar>
 #include <QComboBox>
 #include <array>
+#include <cstdint>
 #include <vector>
 
 struct hid_device_;
@@ -36,23 +37,64 @@ public:
 
 private slots:
     void onPollTimer();
+    void onResetStatsClicked();
 
 private:
+    enum class ExchangeFailure
+    {
+        None,
+        InvalidArgument,
+        SendError,
+        ReceiveError,
+        NoResponse
+    };
+
+    struct ExchangeTiming
+    {
+        int attempts = 0;
+        int responseLength = 0;
+        int64_t elapsedUs = 0;
+        ExchangeFailure failure = ExchangeFailure::None;
+    };
+
+    struct DebugStats
+    {
+        uint64_t pollCount = 0;
+        uint64_t successCount = 0;
+        uint64_t failureCount = 0;
+        uint64_t sendErrorCount = 0;
+        uint64_t receiveErrorCount = 0;
+        uint64_t noResponseCount = 0;
+        uint64_t invalidResponseCount = 0;
+        uint64_t slowPollCount = 0;
+        uint64_t frameBudgetMissCount = 0;
+        uint64_t totalExchangeUs = 0;
+        int64_t lastExchangeUs = 0;
+        int64_t maxExchangeUs = 0;
+        int lastAttempts = 0;
+        int maxAttempts = 0;
+        int lastResponseLength = 0;
+    };
+
     bool openAdapter();
     void closeAdapter();
-    bool exchangeCommand(const unsigned char* command, int commandLength, unsigned char* response, int& responseLength);
+    bool exchangeCommand(const unsigned char* command, int commandLength, unsigned char* response,
+        int& responseLength, ExchangeTiming* timing = nullptr);
     bool setAdapterPollingSuspended(bool suspended);
     bool pollController(uint16_t& buttons, int8_t& xAxis, int8_t& yAxis);
 
     void setupUi();
     void updateButtonIndicators(uint16_t buttons);
     void updateAxisDisplay(int8_t x, int8_t y);
+    void recordPollStats(bool success, const ExchangeTiming& timing, bool invalidResponse);
+    void updateDebugStatsLabels();
 
     hid_device* m_HidDevice = nullptr;
     int m_ReportSize = 63;
     int m_ChannelCount = 1;
     int m_FailedPollCount = 0;
     QTimer* m_PollTimer = nullptr;
+    DebugStats m_DebugStats;
 
     // Button indicators (label + state label pairs)
     struct ButtonIndicator
@@ -71,6 +113,11 @@ private:
 
     QLabel* m_StatusLabel = nullptr;
     QComboBox* m_PortComboBox = nullptr;
+    QLabel* m_StatsPollsLabel = nullptr;
+    QLabel* m_StatsLatencyLabel = nullptr;
+    QLabel* m_StatsSlowPollsLabel = nullptr;
+    QLabel* m_StatsErrorsLabel = nullptr;
+    QLabel* m_StatsResponseLabel = nullptr;
 };
 
 } // namespace UserInterface
